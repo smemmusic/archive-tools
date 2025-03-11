f0f859191da406044541ce676f5b1a586adb6977
Christopher Curtis <ccurtis@gmail.com>
2025-03-11 20:38:10 +0100

    convert

--- bin/mediaphoto.sh
+++ bin/mediaphoto.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 gphoto2 --capture-image 
-magick capt0000.jpg -resize 800 -deskew 40% -fuzz 50% -trim +repage -quality 82 disk.jpg
+convert capt0000.jpg -resize 800 -deskew 40% -fuzz 50% -trim +repage -quality 82 disk.jpg
 serial=$(zbarimg disk.jpg)
 
 echo $serial > serial.txt
