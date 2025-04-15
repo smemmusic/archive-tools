86b4ea00fedda28fae681ffde676153797a8bf6a
Christopher Curtis <ccurtis@gmail.com>
2025-04-15 20:10:03 +0200

    don't convert

--- bin/mediaphoto.sh
+++ bin/mediaphoto.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -x
 
 gphoto2 --capture-image-and-download --force-overwrite
-convert capt0000.jpg -resize 800 -deskew 40% -fuzz 50% -trim +repage -quality 82 disk.jpg
+#convert capt0000.jpg -resize 800 -deskew 40% -fuzz 50% -trim +repage -quality 82 disk.jpg
 serial=$(zbarimg -q --raw capt0000.jpg)
 
 if [[ -z "$serial" ]]; then
@@ -15,6 +15,6 @@ cat serial.txt
 
 mkdir $serial
 
-mv disk.jpg ${serial}/${serial}.jpg
+mv capt0000.jpg ${serial}/${serial}.jpg
 
 echo "insert disk in floppy drive then run dump.sh"
