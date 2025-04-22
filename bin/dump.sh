0c00c6d93205f4be16b41884a3d50e4731cbbab1
Christopher Curtis <ccurtis@gmail.com>
2025-04-22 20:08:49 +0200

    add serial check to dump

--- bin/dump.sh
+++ bin/dump.sh
@@ -1,6 +1,11 @@
 #!/bin/bash -x
 serial=$(cat serial.txt)
 
+if [[ -z $serial ]]; then
+    echo "serial missing"
+    exit 1
+fi
+
 mkdir -p ${serial}
 
-dd if=/dev/usbfd of=${serial}/${serial}.img bs=512
+dd if=/dev/usbfd of=${serial}/${serial}.img bs=512 && rm serial.txt
